148275de6263208626d06f6a93150a2251eeb02c
npmt-events <61161080+npmt-events@users.noreply.github.com>
2020-03-10 02:38:46 +0200

    Update product-by-sub-category.blade.php

--- resources/views/pages/product-by-sub-category.blade.php
+++ resources/views/pages/product-by-sub-category.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div>
-	<div class="row mx-auto justify-content-center py-3" style="max-height:650px;overflow: scroll;">
+	<div class="pb-3 shadow">
 		<p class="lead alert alert-info col-12">
 			<a href="{{ url('/products') }}">Products </a>/
 			<a href="{{ url('/category/'.$cat->category->id) }}"> {{$cat->category->name}} /</a>
@@ -11,9 +11,11 @@
 		<p class="col-12 text-muted text-center">
 			total items : {{ count($products) }}
 		</p>
-		@foreach($products as $product)
-		<x-product-card :product="$product" />
-		@endforeach
+		<div class="row mx-auto justify-content-center" style="max-height:450px;overflow: auto;">
+			@foreach($products as $product)
+			<x-product-card :product="$product" />
+			@endforeach
+		</div>
 	</div>
 </div>
 
